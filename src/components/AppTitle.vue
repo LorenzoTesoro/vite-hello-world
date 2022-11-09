ce2ada2626a873cc9d21ba843e3b8c6febcd8bd0
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-09 15:49:48 +0100

    fix add title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+    <h1>My First App with Vite!</h1>
+</template>
+
+<style>
+h1{
+    text-align: center;
+    padding-top:1rem;
+    padding-bottom:1rem;
+}
+</style>
